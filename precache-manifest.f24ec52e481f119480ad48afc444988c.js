51e58960cb30642f4c8cb9ccb89303d900f0128f
Tazel_Hossan <tazelhossan3@gmail.com>
2020-07-31 23:32:48 +0600

    Updates

---
+++ precache-manifest.f24ec52e481f119480ad48afc444988c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6540ebe6021d26efe393d344f2f695c",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "0c53ce5cf28a16d0ebe0",
+    "url": "/react-project/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "3ed6d915a84fce714faa",
+    "url": "/react-project/static/css/main.8838cd9f.chunk.css"
+  },
+  {
+    "revision": "0c53ce5cf28a16d0ebe0",
+    "url": "/react-project/static/js/2.8e4209b0.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/react-project/static/js/2.8e4209b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed6d915a84fce714faa",
+    "url": "/react-project/static/js/main.8ab0d083.chunk.js"
+  },
+  {
+    "revision": "5e164878bac73f45f380",
+    "url": "/react-project/static/js/runtime-main.fb83cf37.js"
+  }
+]);
